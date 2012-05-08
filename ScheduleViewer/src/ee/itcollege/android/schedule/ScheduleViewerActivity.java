3cb3843304d255c67d4ba22114d18970909fd81b
HeikiTamm <heiki.tamm@itcollege.ee>
2012-05-08 20:24:35 +0300

    added "submit and continue" button to main screen

--- ScheduleViewer/src/ee/itcollege/android/schedule/ScheduleViewerActivity.java
+++ ScheduleViewer/src/ee/itcollege/android/schedule/ScheduleViewerActivity.java
@@ -2,18 +2,21 @@ package ee.itcollege.android.schedule;
 
 
 import android.app.Activity;
-
 import android.os.Bundle;
 
 public class ScheduleViewerActivity extends Activity {
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	/** Called when the activity is first created. 
+	 * 
+	 * @param savedInstanceState
+	 * @param URI
+	 */
+	public void onCreate(Bundle savedInstanceState, String URI) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
 		
-		new FileManager().DownloadFromUrl("https://itcollege.ois.ee/et/schedule/ical?Submit=&student_id=2224", "failinimi");
+		new FileManager()
+				.DownloadFromUrl("https://itcollege.ois.ee/et/schedule?&format=json&student_id=1679", "failinimi");
 		
 		
 	}
