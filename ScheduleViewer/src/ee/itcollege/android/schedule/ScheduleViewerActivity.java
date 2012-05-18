b62798a57eb294f5dbb1c943e7b064c34ecc9660
Liina Vaker <liinavaker@gmail.com>
2012-05-18 16:12:08 +0300

    Vaate päises (listview) kohal näidatakse tänast kuupäeva (yyyy-mm-dd).

--- ScheduleViewer/src/ee/itcollege/android/schedule/ScheduleViewerActivity.java
+++ ScheduleViewer/src/ee/itcollege/android/schedule/ScheduleViewerActivity.java
@@ -1,9 +1,17 @@
 package ee.itcollege.android.schedule;
 
+
+import java.sql.Date;
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.TextView;
 
 public class ScheduleViewerActivity extends FragmentActivity {
+	
 
 
     /** Called when the activity is first created. */
@@ -11,6 +19,21 @@ public class ScheduleViewerActivity extends FragmentActivity {
     public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.main);
+	TextView currently_shown_schedule;
+	currently_shown_schedule = (TextView) findViewById(R.id.currently_shown_schedule);
+	String showtext = getDate().toString();
+	currently_shown_schedule.setText(showtext);
+    }
+    
+    public Date getDate() {
+    	// Get today as a Calendar  
+		Calendar today = Calendar.getInstance();  
+		// Subtract 1 day  
+		today.add(Calendar.DATE, -1);  
+		// Make an SQL Date out of that  
+		java.sql.Date yesterday = new java.sql.Date(today.getTimeInMillis()); 
+    	
+		return yesterday;
     }
    
  /*   
