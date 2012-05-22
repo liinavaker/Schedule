c7d1a9133b4f58ede064d9003be0e8b5f311a4c8
Liina Vaker <liinavaker@gmail.com>
2012-05-22 14:55:44 +0300

    Toimib "jäta mind meelde" funktsioon.
    Kui kasutaja check'ib kasti "jäta mind meelde", siis tehakse SD-kaardile
    Schedule kausta uus fail nimega userID.txt, kuhu kirjutatakse meelde
    jäetud kasutaja ID. Kui kasutaja võtab kastist "jäta mind meelde"
    linnukese ära, siis fail kustutatakse. Kui tahetakse meelde jätta uus
    ID, siis vana fail kustutakse ja kirjutatakse uus.

--- ScheduleViewer/src/ee/itcollege/android/schedule/FrontpageActivity.java
+++ ScheduleViewer/src/ee/itcollege/android/schedule/FrontpageActivity.java
@@ -1,34 +1,105 @@
 package ee.itcollege.android.schedule;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class FrontpageActivity extends Activity {
-	
+
+	File sdcard = Environment.getExternalStorageDirectory();
+	File dir = new File(sdcard.getAbsolutePath() + "/Schedule");
+	String filename = "userID" + ".txt";
+	File file = new File(dir, filename);
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.front);
-		
+		File sdcard = Environment.getExternalStorageDirectory();
+		File dir = new File(sdcard.getAbsolutePath() + "/Schedule");
+
+		EditText edittext = (EditText) findViewById(R.id.userID);
+
+		CheckBox checkbox = (CheckBox) findViewById(R.id.rememberMe);
+
+		if (dir.exists()) {
+			try {
+				String result = readFile();
+				if (result != "") {
+					edittext.setText(result);
+					checkbox.setChecked(true);
+				}
+				// TODO: loe faili sisu ja EditText.setText()
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} else
+			checkbox.setChecked(false);
+
 	}
-	
-	//TODO: Kui nime pole valitud, siis nuppu vajutada ei saa!
-	
-	public void onButtonClicked (View view) {
+
+	public void writeToSDCard(String userID) {
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+
+		if (file.exists()) {
+			file.delete();
+		}
+
+		BufferedWriter br = null;
+
+		try {
+			br = new BufferedWriter(new FileWriter(file));
+			br.write(userID);
+			br.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (null != br)
+				try {
+					br.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+	}
+
+	private String readFile() throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		return br.readLine();
+	}
+
+	public void onButtonClicked(View view) {
 		EditText edittext = (EditText) findViewById(R.id.userID);
 		String input = edittext.getText().toString();
-		
-		
+
+		CheckBox checkbox = (CheckBox) findViewById(R.id.rememberMe);
+		if (checkbox.isChecked()) {
+			writeToSDCard(input);
+		}
+		if (!checkbox.isChecked() && file.exists()) {
+			file.delete();
+		}
+
 		Intent intent = new Intent(this, ScheduleViewerActivity.class);
 		EventListFragment.userID = input;
-	
-	//	intent.putExtra(EventListFragment.userID, input);
-		startActivity(intent);	
+		startActivity(intent);
 	}
 }
